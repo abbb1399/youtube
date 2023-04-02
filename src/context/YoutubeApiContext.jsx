e3c8c5756ecbe2c51cb395113ffb791ff479c638
paul <abbb123@naver.com>
2023-04-02 16:42:51 +0900

    완성

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,11 +1,12 @@
 import { createContext, useContext } from "react";
 import Youtube from "../api/youtube";
 import YoutubeClient from "../api/youtubeClient";
+import FakeYoutbeClient from "../api/fakeYoutubeClient";
 
 export const YoutubeApiContext = createContext();
 
-// const client = new FakeYoutube();
-const client = new YoutubeClient();
+const client = new FakeYoutbeClient();
+// const client = new YoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
